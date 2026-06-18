f81ac943dbc6301407346f989ec3250909caf0a1
LowHoang1104 <long110604@gmail.com>
2026-06-18 15:34:36 +0700

    text

--- Joss-paper-web/VietMa/src/components/Layout.jsx
+++ Joss-paper-web/VietMa/src/components/Layout.jsx
@@ -143,6 +143,26 @@ function Layout() {
                 <br />
                 7:00 - 19:00 (Thứ 2 - CN)
               </p>
+
+              <a
+                href="https://www.facebook.com/profile.php?id=61590306604248"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="footer-facebook-link"
+                style={{ display: 'inline-flex', alignItems: 'center', gap: '8px', color: 'inherit', textDecoration: 'none', marginTop: '8px' }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path d="M24 12.073C24 5.405 18.627 0 12 0S0 5.405 0 12.073C0 18.1 4.388 23.094 10.125 24v-8.437H7.078v-3.49h3.047V9.41c0-3.025 1.792-4.697 4.533-4.697 1.312 0 2.686.236 2.686.236v2.97h-1.513c-1.491 0-1.956.93-1.956 1.886v2.268h3.328l-.532 3.49h-2.796V24C19.612 23.094 24 18.1 24 12.073z" />
+                </svg>
+                Facebook Việt Mã
+              </a>
             </div>
           </div>
 
